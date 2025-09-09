a0f08fcbe062d8937c592cfdb7c1d72981113fbe
column-wise <cmh1199@naver.com>
2025-09-10 00:06:09 +0900

    fix: Add missing RedisTemplate bean configuration

--- BE/src/main/java/io/github/columnwise/shortlink/config/RedisConfig.java
+++ BE/src/main/java/io/github/columnwise/shortlink/config/RedisConfig.java
@@ -1,7 +1,35 @@
 package io.github.columnwise.shortlink.config;
 
+import io.github.columnwise.shortlink.domain.model.ShortUrl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, ShortUrl> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, ShortUrl> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> objectRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
 }
