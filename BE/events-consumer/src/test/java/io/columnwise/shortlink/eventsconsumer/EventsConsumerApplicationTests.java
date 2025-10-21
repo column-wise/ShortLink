cdf9e552c981ea44119418731cc5b7311d680096
column-wise <columnwise99@gmail.com>
2025-10-21 22:55:31 +0900

    refactor: Update project files and configs

---
+++ BE/events-consumer/src/test/java/io/columnwise/shortlink/eventsconsumer/EventsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.columnwise.shortlink.eventsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
